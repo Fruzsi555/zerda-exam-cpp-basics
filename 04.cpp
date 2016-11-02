8a52f511a00c2d0274e3110cb5b81fd52101a21c
Fruzsi555 <karman.fruzsina@gmail.com>
2016-11-02 11:12:21 +0100

    Exam tasks 1-4

--- 04.cpp
+++ 04.cpp
@@ -10,8 +10,8 @@ int numbers_divisors(int number) {
   int counter = 0;
   for (int i = 1; i <= number; i++) {
     if (number % i == 0) {
-	  counter++;
-	}
+      counter++;
+  }
   }
   return counter;
 }
